bd2a49dc3c7299b620327dae875627d02b35f1ad
ezmcgse <sean.s.mcgrath@ericsson.com>
2018-10-13 11:44:44 +0100

    finalzing code

--- deter-inver-cross.py
+++ deter-inver-cross.py
@@ -11,7 +11,7 @@ class DeterminantInverse:
 
     #function calculates the determinant of a 2x2 matrix
     def cal_determinant(self, mat):
-        #implementation from rick in the forums
+        #implementation from rick suggestion  in the forums
         if len(mat) != 2 and len(mat[0] != 2):
                 raise Exception(
                     "\nMatrix must be of size 2x2 to calculate determinant.")
@@ -32,24 +32,23 @@ class DeterminantInverse:
             #divide by 1
             fracDet = 1/det
 
-        try:
-            if len(mat[0]) == 2 and len(mat[1]) == 2:
-                #changes the places of matrix
-                tmp = mat[0][0]
-                mat[0][0] = mat[1][1]
-                mat[1][1] = tmp
-                #changes from positive to negative and vice versa
-                mat[0][1] = self.check_for_neg(mat[0][1])
-                mat[1][0] = self.check_for_neg(mat[1][0])
-                
-                for i in range(len(mat[0])):
-                    for j in range(len(mat[1])):
-                        inverseMat[i][j] += fracDet * mat[i][j]
-        except:
+        if len(mat[0]) == 2 and len(mat[1]) == 2:
+            #changes the places of matrix
+            tmp = mat[0][0]
+            mat[0][0] = mat[1][1]
+            mat[1][1] = tmp
+            #changes from positive to negative and vice versa
+            mat[0][1] = self.check_for_neg(mat[0][1])
+            mat[1][0] = self.check_for_neg(mat[1][0])
+            
+            for i in range(len(mat[0])):
+                for j in range(len(mat[1])):
+                    inverseMat[i][j] += fracDet * mat[i][j]
+            
+            return inverseMat
+        else:
             print(
                 '\nINFO - INPUT MISMATCH: Inverse can only be calculated on a 2X2 matrix')
-        finally:
-            return inverseMat
 
     def check_for_neg(self, x):
         newNum = 0
@@ -131,14 +130,14 @@ class MatrixVectorMulti:
     def matrice_x_vector(self, mat, vect):
         matLen = len(mat)
         vetLen = len(vect)
-        #list gets populated with multiplication values
-        multiAns = []
-        #list gets populated with sum of multiplication values
+    
         sumAns = [[0 for y in range(vetLen)]]
         try:
             if len(mat[0]) == vetLen:
                 for i in range(matLen):
                     for j in range(vetLen):
+                        #remodeled this calculation based pn Declan 
+                        #Staunton's code.
                         sumAns[0][i] += mat[i][j] * vect[j]
         except:
             print(
@@ -177,11 +176,7 @@ if __name__ == '__main__':
 
     answerDeter = 8
     answerInverse = [[1.5, -2.0], [-0.5, 1.0]]
-<<<<<<< HEAD
     answerMatVec = [[5, -91, -66, 2]]
-=======
-    answerMatVec = [5, -91, -66, 2]
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
     answerCrossProd = [-30, 1, 37]
     answerMatAdd = [[3, 0, 13, 1], [8, 1, 7, 3],
                     [8, 1, 7, 14], [8, -6, 13, -10]]
@@ -192,93 +187,58 @@ if __name__ == '__main__':
                      [52, -18, 67, 45],
                      [-12, 14, -13, 61]]
 
-<<<<<<< HEAD
     if answerDeter == DeterminantInverse(mat).cal_determinant(mat):
-=======
-    #print(DeterminantInverse(mat).calDeterminant(mat))
-    print(DeterminantInverse(matInverse).inverseOfMatrix(matInverse))
-
-    if answerDeter == DeterminantInverse(mat).calDeterminant(mat):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
         print('1). Determinant of {} = {} == True\n'.format(
             mat, answerDeter))
     else:
         print('False')
 
-<<<<<<< HEAD
     #For some reason this test returns false as we can see from the else its correct
     #if someone can have a look that would be great
     if answerInverse == DeterminantInverse(matInverse).inverse_of_matrix(matInverse):
-=======
-    #For some reason this returns false as we can see from the else its correct
-    #if someone can have a look that would be great
-    if answerInverse == DeterminantInverse(matInverse).inverseOfMatrix(matInverse):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
         print('2). \nInverse of {} = {}  == True\n'.format(
             matInverse, answerInverse))
     else:
         print('2). == False')
         print(answerInverse == DeterminantInverse(
-<<<<<<< HEAD
             matInverse).inverse_of_matrix(matInverse))
 
     if answerInverse == DeterminantInverse(mat2).inverse_of_matrix(mat2):
-=======
-            matInverse).inverseOfMatrix(matInverse))
-
-    if answerInverse == DeterminantInverse(mat2).inverseOfMatrix(mat2):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
         print('3). \nInverse of {} = {}  == True\n'.format(
             matInverse, answerInverse))
     else:
         print('3). == False\n')
 
-<<<<<<< HEAD
     if answerMatAdd == AddOrSubMatrix(matrix3, matrix4).matrix_add_subtr(matrix3, matrix4, add):
-=======
-    if answerMatAdd == AddOrSubMatrix(matrix3, matrix4).matrixAddOrSubtr(matrix3, matrix4, add):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
+        print('\n =========== 4x4 Matrix addition ============')
         print('4). {} \n + {} \n= {}  == True\n'.format(
             matrix3, matrix4, answerMatAdd))
     else:
         print('4). == False')
 
-<<<<<<< HEAD
     if answerMatSub == AddOrSubMatrix(matrix3, matrix4).matrix_add_subtr(matrix3, matrix4, sub):
-=======
-    if answerMatSub == AddOrSubMatrix(matrix3, matrix4).matrixAddOrSubtr(matrix3, matrix4, sub):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
+        print('\n =========== 4x4 Matrix substraction ============')
         print('5). {} \n - {} \n= {}  == True\n'.format(
             matrix3, matrix4, answerMatSub))
     else:
         print('5). == False')
 
-<<<<<<< HEAD
     if answerMatMulti == MatrixMulti(matrix3, matrix4).matrice_multi(matrix3, matrix4):
-=======
-    if answerMatMulti == MatrixMulti(matrix3, matrix4).matriceMulti(matrix3, matrix4):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
+        print('\n =========== 4x4 Matrix multiplication ============')
         print('6). {} \n X {} \n= {}  == True\n'.format(
             matrix3, matrix4, answerMatSub))
     else:
         print('6). == False')
 
-<<<<<<< HEAD
     if answerMatVec == MatrixVectorMulti(testMat4, vect4).matrice_x_vector(testMat4, vect4):
-=======
-    if answerMatVec == MatrixVectorMulti(testMat4, vect4).matriceXVector(testMat4, vect4):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
+        print('\n ====== 4x4 Matrix multiply 1x4 Vector ============')
         print('7). {} \n X {} \n= {}  == True\n'.format(
             testMat4, vect4, answerMatVec))
     else:
         print('7). == False')
 
     if answerCrossProd == CrossProduct(inverseVector1,
-<<<<<<< HEAD
                        inverseVector2).calculate_cross_product(inverseVector1, inverseVector2):
-=======
-                       inverseVector2).calculateCrossProduct(inverseVector1, inverseVector2):
->>>>>>> 1589414fd0f558e80ddb070c5b5b06648e0ffc09
         print('8). Cross Product of \n{} And {} = {}  == True\n'.format(
             inverseVector1, inverseVector2, answerCrossProd))
     else:
